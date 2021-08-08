eb33bf6103fd3fe84b64350dcaa71522fdfacb35
Avinash Dengani <avinashdengani123@gmail.com>
2021-08-08 19:54:16 +0530

    Managed Exception Handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,24 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ResponseHelper;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ResponseHelper;
+    const FOREIGN_KEY_VIOLATION_CODE = 1451;
+    const DB_CONNECTION_FAILED = 2002;
     /**
      * A list of the exception types that are not reported.
      *
@@ -27,6 +40,39 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
+    public function render($request, Throwable $e)
+    {
+        if($e instanceof ValidationException) {
+            return $this->convertValidationExceptionToResponse($e, $request);
+        } else if($e instanceof ModelNotFoundException) {
+            $className = class_basename($e->getModel());
+            return $this->errorResponse("$className does not exist with the specified key", 404);
+        } else if($e instanceof AuthenticationException) {
+            return $this->unauthenticated($request,  $e);
+        }else if($e instanceof AuthorizationException) {
+            return $this->errorResponse($e->getMessage(), 403);
+        }else if($e instanceof NotFoundHttpException) {
+            return $this->errorResponse("The specified URL cannot be found!", 404);
+        }else if($e instanceof MethodNotAllowedHttpException) {
+            return $this->errorResponse("The specified method for this URL cannot be found!", 405);
+        }else if($e instanceof HttpException) {
+            return $this->errorResponse($e->getMessage(), $e->getStatusCode());
+        }else if($e instanceof QueryException) {
+            $sqlErrorCode = $e->errorInfo[1];
+            if($sqlErrorCode == self::FOREIGN_KEY_VIOLATION_CODE) {
+                return $this->errorResponse("Cannot remove this resource permanently, as it is referred by some other resource", 409);
+            } else if($sqlErrorCode == self::DB_CONNECTION_FAILED) {
+                return $this->errorResponse("Out database server is down! Please try after sometime", 500);
+            }
+        }
+
+        if(config('app.debug')) {
+            return parent::render($request, $e);
+        }
+
+        return $this->errorResponse("Unexpected server error", 500);
+    }
+
     /**
      * Register the exception handling callbacks for the application.
      *
@@ -37,5 +83,13 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+    protected function convertValidationExceptionToResponse(ValidationException $e, $request)
+    {
+        return $this->reportMultipleErrors($e->errors(), 422);
+    }
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $this->errorResponse("unauthenticated", 401);
     }
 }
