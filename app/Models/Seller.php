6092ba5a961a22e8a7b185e7826da8e984ee4b62
Avinash Dengani <avinashdengani123@gmail.com>
2021-08-08 11:45:59 +0530

    Extended User class and overide table

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Seller extends Model
+class Seller extends User
 {
     use HasFactory;
+
+    protected $table = 'users';
 }
