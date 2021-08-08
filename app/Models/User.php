cc0b177d97e45282a400d01e651ab3a5b401e00d
Avinash Dengani <avinashdengani123@gmail.com>
2021-08-08 18:47:31 +0530

    Added Getters and Mutators

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,4 +65,18 @@ class User extends Authenticatable
     {
         return Str::random(40);
     }
+    // GETTERS
+    public function getNameAttribute()
+    {
+        return ucwords($this->attributes['name']);
+    }
+    // MUTATORS
+    public function setNameAttribute(string $name)
+    {
+        $this->attributes['name'] = strtolower($name);
+    }
+    public function setEmailAttribute(string $email)
+    {
+        $this->attributes['email'] = strtolower($email);
+    }
 }
