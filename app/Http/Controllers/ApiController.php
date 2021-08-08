02300ec8f456372b7b558ddcc9eaaceb0bbfdc3b
Avinash Dengani <avinashdengani123@gmail.com>
2021-08-08 18:46:18 +0530

    Crated a ApiController to use ResponsiveHelper Trait

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Traits\ResponseHelper as TraitsResponseHelper;
+use App\Triats\ResponseHelper;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    use TraitsResponseHelper;
+}
