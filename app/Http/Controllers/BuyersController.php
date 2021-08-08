312d5eade5eb87d9e0abe21595456d7cb8fe113d
Avinash Dengani <avinashdengani123@gmail.com>
2021-08-08 17:54:37 +0530

    Added functionality to Buyers and Sellers Controller to get all buyers and sellers and to get specific buyer or seller

--- app/Http/Controllers/BuyersController.php
+++ app/Http/Controllers/BuyersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Buyer;
 use Illuminate\Http\Request;
 
 class BuyersController extends Controller
@@ -13,28 +14,8 @@ class BuyersController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+        $buyers = Buyer::has('transactions')->get();
+        return response()->json(['count' => $buyers->count() ,'data' => $buyers], 200);
     }
 
     /**
@@ -45,40 +26,7 @@ class BuyersController extends Controller
      */
     public function show($id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $buyer = Buyer::has('transactions')->findOrFail($id);
+        return response()->json(['data' => $buyer], 200);
     }
 }
