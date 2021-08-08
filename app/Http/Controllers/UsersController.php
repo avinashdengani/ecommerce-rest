169e6f8ab454d10efc78fa0adcb26960a9e81bb3
Avinash Dengani <avinashdengani123@gmail.com>
2021-08-08 17:32:23 +0530

    Added functionality to UsersController to get all users, show specific user, store new user, update user and delete user

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -13,17 +14,8 @@ class UsersController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $users = User::all();
+        return response()->json(['data' => $users], 200);
     }
 
     /**
@@ -34,7 +26,21 @@ class UsersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = [
+            'name' => 'required|min:5',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|confirmed'
+        ];
+        $this->validate($request, $rules);
+
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);
+        $data['verified'] = User::UNVERIFIED_USER;
+        $data['verification_token'] = User::generateVerificationCode();
+        $data['admin'] = User::REGULAR_USER;
+
+        $user = User::create($data);
+        return response()->json(['data' => $user], 201);
     }
 
     /**
@@ -43,20 +49,9 @@ class UsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        return response()->json(['data' => $user], 200);
     }
 
     /**
@@ -66,19 +61,50 @@ class UsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, User $user)
     {
-        //
-    }
+        $rules = [
+            'name' => 'min:5',
+            'email' => 'email|unique:users',
+            'password' => 'min:8|confirmed',
+            'admin' => 'in:'. User::REGULAR_USER . ',' . User::ADMIN_USER
+        ];
+        $this->validate($request, $rules);
 
+        if($request->has('name')) {
+            $user->name = $request->name;
+        }
+        if($request->has('email')) {
+            $user->verified = User::UNVERIFIED_USER;
+            $user->verification_token = User::generateVerificationCode();
+            $user->email = $request->email;
+        }
+        if($request->has('password')) {
+            $user->password = bcrypt($request->password);
+        }
+        if($request->has('admin')) {
+            if(! $user->isVerified()) {
+                return response()->json(['error' => 'Only verified users can be admin!', 'code' => 409], 409);
+            }
+            $user->admin = $request->admin;
+        }
+        if(! $user->isDirty()) {
+            return response()->json(['error' => 'You need to specify some differnet value to update', 'code' => 422], 422);
+        }
+
+        $user->save();
+
+        return response()->json(['data' => $user], 200);
+    }
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+        return response()->json(['data' => $user], 200);
     }
 }
