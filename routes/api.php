3d0162b79e742ba5cae35aa5ec404316f1feeb2f
Avinash Dengani <avinashdengani123@gmail.com>
2021-08-08 12:13:17 +0530

    Added api routes for buyers

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,11 @@
 <?php
 
+use App\Http\Controllers\BuyersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
+Route::resource('buyers', BuyersController::class)->only('index', 'show');
+/**
+ * Another way for using same routes written above for buyers
+ * Route::resource('buyers', BuyersController::class, ['only' => ['index', 'show']]);
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
