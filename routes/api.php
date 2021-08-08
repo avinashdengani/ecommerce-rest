271f8dc22337fb2e6ec0df5e2e2b667ede1690f9
Avinash Dengani <avinashdengani123@gmail.com>
2021-08-08 17:53:57 +0530

    Added api routes for sellers

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BuyersController;
+use App\Http\Controllers\SellersController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,4 +11,5 @@ Route::resource('buyers', BuyersController::class)->only('index', 'show');
  * Another way for using same routes written above for buyers
  * Route::resource('buyers', BuyersController::class, ['only' => ['index', 'show']]);
 */
+Route::resource('sellers', SellersController::class)->only('index', 'show');
 Route::resource('users', UsersController::class)->except('create', 'edit');
