e0339c8937d90da8e7b725d5ca0132cf8b42e2c4
Avinash Dengani <avinashdengani123@gmail.com>
2021-09-04 20:28:49 +0530

    Added api routes for transactions.categories, transactions.sellers, buyers.transactions, buyers.products, buyers.sellers, buyers.categories

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Http\Controllers\BuyerCategoryController;
+use App\Http\Controllers\BuyerProductController;
 use App\Http\Controllers\BuyersController;
+use App\Http\Controllers\BuyerSellerController;
+use App\Http\Controllers\BuyerTransactionController;
 use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\SellersController;
+use App\Http\Controllers\TransactionCategoryController;
+use App\Http\Controllers\TransactionSellerController;
 use App\Http\Controllers\TransactiosController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
@@ -17,5 +23,13 @@ Route::resource('buyers', BuyersController::class)->only('index', 'show');
 Route::resource('sellers', SellersController::class)->only('index', 'show');
 Route::resource('users', UsersController::class)->except('create', 'edit');
 Route::resource('categories', CategoriesController::class)->except('create', 'edit');
-Route::resource('transactions', TransactiosController::class)->only('index', 'show');
 Route::resource('products', ProductsController::class)->only('index', 'show');
+Route::resource('transactions', TransactiosController::class)->only('index', 'show');
+
+
+Route::resource('transactions.categories', TransactionCategoryController::class)->only('index');
+Route::resource('transactions.sellers', TransactionSellerController::class)->only('index');
+Route::resource('buyers.transactions', BuyerTransactionController::class)->only('index');
+Route::resource('buyers.products', BuyerProductController::class)->only('index');
+Route::resource('buyers.sellers', BuyerSellerController::class)->only('index');
+Route::resource('buyers.categories', BuyerCategoryController::class)->only('index');
