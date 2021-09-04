92a8165a0303ab7de4ebdd939e99a1891508e3dc
Avinash Dengani <avinashdengani123@gmail.com>
2021-09-04 20:24:01 +0530

    Created a controller to get all the products associated to particular Buyer

---
+++ app/Http/Controllers/BuyerProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Buyer;
+use Illuminate\Http\Request;
+
+class BuyerProductController extends ApiController
+{
+    public function index(Buyer $buyer)
+    {
+        $products =  $buyer->transactions()
+                ->with('product')
+                ->get()
+                ->pluck('product');
+        return $this->showAll($products);
+    }
+}
