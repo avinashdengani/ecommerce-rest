4e933750c156e45a03055f9cd146e969d68574bb
Avinash Dengani <avinashdengani123@gmail.com>
2021-09-04 20:25:27 +0530

    Created a Contoller to get all the transactions associated with specific Buyer

---
+++ app/Http/Controllers/BuyerTransactionController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Buyer;
+use Illuminate\Http\Request;
+
+class BuyerTransactionController extends ApiController
+{
+    public function index(Buyer $buyer)
+    {
+        $transactions =  $buyer->transactions;
+        return $this->showAll($transactions);
+    }
+}
