91984b0febf428fc9db403c0f062d29056a617fe
Avinash Dengani <avinashdengani123@gmail.com>
2021-09-04 20:24:52 +0530

    Created a controller to get all the Sellers associated to spcific buyer

---
+++ app/Http/Controllers/BuyerSellerController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Buyer;
+use Illuminate\Http\Request;
+
+class BuyerSellerController extends ApiController
+{
+    public function index(Buyer $buyer)
+    {
+        $sellers =  $buyer->transactions()
+                ->with('product.seller')
+                ->get()
+                ->pluck('product.seller')
+                ->unique();
+        return $this->showAll($sellers);
+    }
+}
