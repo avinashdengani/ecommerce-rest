7bf4fed37538fc4c0475ac43d813824c93199436
Avinash Dengani <avinashdengani123@gmail.com>
2021-09-12 15:49:49 +0530

    Displaying all Buyers associated with a seller

---
+++ app/Http/Controllers/SellerBuyerController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Seller;
+use Illuminate\Http\Request;
+
+class SellerBuyerController extends ApiController
+{
+    public function index(Seller $seller)
+    {
+        $buyers = $seller->products()
+                        ->whereHas('transactions')
+                        ->with('transactions.buyer')
+                        ->get()
+                        ->pluck('transactions')
+                        ->flatten()
+                        ->pluck('buyer')
+                        ->unique('id')
+                        ->values();
+
+        return $this->showAll($buyers);
+    }
+}
