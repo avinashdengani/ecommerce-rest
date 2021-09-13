73ad2058df300b620cd035c6a06b87d4da17c3d3
Avinash Dengani <avinashdengani123@gmail.com>
2021-09-13 18:51:30 +0530

    Added api routes to verify email id using verification_token which will sent to user via email

--- routes/api.php
+++ routes/api.php
@@ -68,3 +68,5 @@ Route::resource('transactions.sellers', TransactionSellerController::class)->onl
 
 //USER
 Route::resource('users', UsersController::class)->except('create', 'edit');
+Route::get('users/verify/{token}', [UsersController::class, 'verify'])->name('users.verify');
+Route::get('users/{user}/resend-verification-email', [UsersController::class, 'resend'])->name('users.resend');
