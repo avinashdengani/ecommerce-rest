3e2d9ab74f0cc6ec5092cacb173b0054d58a58ee
Avinash Dengani <avinashdengani123@gmail.com>
2021-09-13 22:35:11 +0530

    Added transformer  class in Category Model and added columns to transform in CategoryTransformer

---
+++ app/Transformers/CategoryTransformer.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Models\Category;
+use League\Fractal\TransformerAbstract;
+
+class CategoryTransformer extends TransformerAbstract
+{
+    /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected $defaultIncludes = [
+        //
+    ];
+
+    /**
+     * List of resources possible to include
+     *
+     * @var array
+     */
+    protected $availableIncludes = [
+        //
+    ];
+
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(Category $category)
+    {
+        return [
+            'identifier' => (int)$category->id,
+            'title' => $category->name,
+            'details' => $category->description,
+            'creationDate' => $category->created_at,
+            'lastChangedDate' => $category->updated_at,
+            'deletionDate' => $category->deleted_at ?? null,
+        ];
+    }
+}
