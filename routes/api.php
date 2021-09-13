ffc10486d86d04d682b36d84d1c410c7965a4201
Avinash Dengani <avinashdengani123@gmail.com>
2021-09-13 18:43:20 +0530

    Added api routes for products

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,11 @@ use App\Http\Controllers\Category\CategoryBuyerController;
 use App\Http\Controllers\Category\CategoryProductController;
 use App\Http\Controllers\Category\CategorySellerController;
 use App\Http\Controllers\Category\CategoryTransactionController;
+use App\Http\Controllers\Product\ProductBuyerController;
+use App\Http\Controllers\Product\ProductBuyerTransactionController;
+use App\Http\Controllers\Product\ProductCategoryController;
 use App\Http\Controllers\Product\ProductsController;
+use App\Http\Controllers\Product\ProductTransactionController;
 use App\Http\Controllers\Seller\SellerBuyerController;
 use App\Http\Controllers\Seller\SellerCategoryController;
 use App\Http\Controllers\Seller\SellerProductController;
@@ -43,10 +47,6 @@ Route::resource('sellers.buyers', SellerBuyerController::class)->only('index');
 Route::resource('sellers.categories', SellerCategoryController::class)->only('index');
 Route::resource('sellers.products', SellerProductController::class)->except(['create', 'edit']);
 
-
-//USER
-Route::resource('users', UsersController::class)->except('create', 'edit');
-
 //CATEGORY
 Route::resource('categories', CategoriesController::class)->except('create', 'edit');
 Route::resource('categories.products', CategoryProductController::class)->only('index');
@@ -54,10 +54,17 @@ Route::resource('categories.sellers', CategorySellerController::class)->only('in
 Route::resource('categories.buyers', CategoryBuyerController::class)->only('index');
 Route::resource('categories.transactions', CategoryTransactionController::class)->only('index');
 
-
+// PRODUCTS
 Route::resource('products', ProductsController::class)->only('index', 'show');
+Route::resource('products.buyers', ProductBuyerController::class)->only('index');
+Route::resource('products.categories', ProductCategoryController::class)->only('index', 'update', 'destroy');
+Route::resource('products.transactions', ProductTransactionController::class)->only('index');
+Route::resource('products.buyers.transactions', ProductBuyerTransactionController::class)->only('store');
 
 //TRANSACTIONS
 Route::resource('transactions', TransactiosController::class)->only('index', 'show');
 Route::resource('transactions.categories', TransactionCategoryController::class)->only('index');
 Route::resource('transactions.sellers', TransactionSellerController::class)->only('index');
+
+//USER
+Route::resource('users', UsersController::class)->except('create', 'edit');
